ce47a8abff0aa11a63c710b5459783552b7eaeb3
AndreaPio <andreapiovicinanza@gmail.com>
2023-12-13 00:32:15 +0100

    ComplexNumber class

--- src/main/java/unisa/ingsoft/ComplexNumber/ComplexNumber.java
+++ src/main/java/unisa/ingsoft/ComplexNumber/ComplexNumber.java
@@ -1,13 +1,127 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package unisa.ingsoft.ComplexNumber;
 
-/**
- *
- * @author andre
- */
-public class ComplexNumber {
+import java.util.Locale;
+
+import unisa.ingsoft.Exceptions.SyntaxException;
+
+public class ComplexNumber implements Operations<ComplexNumber> {
+    private final double re;
+    private final double im;  
+    //gli attributi sono entrambi final poichè non è prevista la modifica della singola parte reale o immaginaria
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
     
+    @Override
+    public ComplexNumber sum(ComplexNumber t) {
+        double a=this.getRe(); double b=this.getIm(); double c=t.getRe(); double d=t.getIm();
+        return new ComplexNumber(a+c,b+d);
+    }
+
+    @Override
+    public ComplexNumber sub(ComplexNumber t) {
+        double a=this.getRe(); double b=this.getIm(); double c=t.getRe(); double d=t.getIm();
+        return new ComplexNumber(a-c,b-d);
+    }
+
+    @Override
+    public ComplexNumber mol(ComplexNumber t) {
+        double a=this.getRe(); double b=this.getIm(); double c=t.getRe(); double d=t.getIm();
+    return new ComplexNumber(a * c - b * d, a * d + b * c);
+    }
+
+    @Override
+    public ComplexNumber div(ComplexNumber t)throws ArithmeticException{
+        double a=this.getRe(); double b=this.getIm(); double c=t.getRe(); double d=t.getIm();
+        if(c==0 & d==0)
+            throw new ArithmeticException();
+        double div = c * c + d * d;
+        return new ComplexNumber((a * c + b * d) / div, ((b * c - a * d) / div));
+    }
+
+    @Override
+    public ComplexNumber sqrt() {
+        double a=this.getRe(); double b=this.getIm();
+        if(a==0 && b==0)
+            return this;
+        if(b==0){
+            return new ComplexNumber(Math.sqrt(a),0);
+        }
+        double modulo = Math.sqrt(a*a + b*b);
+        double argomento = Math.atan2(b,a)/2.0;
+        return new ComplexNumber(Math.sqrt(modulo) * Math.cos(argomento),
+                (Math.sqrt(modulo) * Math.sin(argomento)));
+    }
+
+    @Override
+    public ComplexNumber inv() {
+        return new ComplexNumber(-1*this.getRe(),-1*this.getIm());
+    }
+    
+    public ComplexNumber conj() {
+        return new ComplexNumber(this.getRe(),-1*this.getIm());
+    }
+
+    @Override
+    public String toString() {
+        String ree=((re%1.0!=0) ? String.format(Locale.US,"%.3f", re) : String.format(Locale.US,"%.0f", re));
+        String imm=((im%1.0!=0) ? String.format(Locale.US,"%.3f", im) : String.format(Locale.US,"%.0f", im));
+        if (im==0) 
+            return ree;
+        if (re==0)
+            return imm+"j";
+        if(im>0)
+            return ree + "+" + imm + "j";
+        else
+            return ree + imm + "j";
+    }
+
+    public static ComplexNumber parseComplexNumber(String input) throws SyntaxException{
+
+        double realPart = 0;
+        double imgPart = 0;
+
+        if (input.matches("^[+-]?\\d+(\\.\\d+)?$")){ // Solo parte reale
+            realPart = Double.parseDouble(input);
+        }else if (input.matches("^[+-]?\\d+(\\.\\d+)?j$")){ //Solo parte immaginaria (j alla fine)
+            String imgString = input.replaceAll("j", "");
+            imgPart = Double.parseDouble(imgString);
+        }else if (input.matches("^[+-]?j\\d+(\\.\\d+)?$")){ //Solo parte immaginaria (j all'inizio)
+            String imgString = input.replaceAll("j", "");
+            imgPart = Double.parseDouble(imgString);
+        }else if (input.matches("[+-]?j")){ //Solo unità immaginaria
+            String imgString = input.replaceAll("j", "1");
+            imgPart = Double.parseDouble(imgString);
+        }else if (input.matches("^[+-]?\\d+(\\.\\d+)?[+-]{1}\\d+(\\.\\d+)?j$")){ //Numero complesso con parte reale e immaginaria (j alla fine)
+            String[] parts = input.split("(?=[+-])");
+            realPart = Double.parseDouble(parts[0]);
+            String imgString = parts[1].replaceAll("j", "");
+            imgPart = Double.parseDouble(imgString);
+        }else if (input.matches("^[+-]?\\d+(\\.\\d+)?[+-]{1}j\\d+(\\.\\d+)?$")){ //Numero complesso con parte reale e immaginaria (j all'inizio)
+            String[] parts = input.split("(?=[+-])");
+            realPart = Double.parseDouble(parts[0]);
+            String imgString = parts[1].replaceAll("j", "");
+            imgPart = Double.parseDouble(imgString);
+        }else if (input.matches("^[+-]?\\d+(\\.\\d+)?[+-]{1}j$")){ //Numero complesso con parte reale e unità immaginaria
+            String[] parts = input.split("(?=[+-])");
+            realPart = Double.parseDouble(parts[0]);
+            String imgString = parts[1].replaceAll("j", "1");
+            imgPart = Double.parseDouble(imgString);
+        }else{
+            throw new SyntaxException();
+        }
+
+        return new ComplexNumber(realPart, imgPart);
+    }
 }
+
