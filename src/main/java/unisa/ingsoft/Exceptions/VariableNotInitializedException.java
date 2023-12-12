2d448ae9690424f291b047bfca7cebd63d46a74d
Tyusibo <a.tudino@studenti.unisa.it>
2023-12-13 00:25:41 +0100

    Exceptions

--- src/main/java/unisa/ingsoft/Exceptions/VariableNotInitializedException.java
+++ src/main/java/unisa/ingsoft/Exceptions/VariableNotInitializedException.java
@@ -1,27 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
- */
 package unisa.ingsoft.Exceptions;
 
-/**
- *
- * @author andre
- */
-public class VariableNotInitializedException {
-
-    /**
-     * Creates a new instance of <code>VariableNotInitializedException</code>
-     * without detail message.
-     */
-    public VariableNotInitializedException() {
+public class VariableNotInitializedException extends Exception{
+    public String getMessage(){
+        return "Variabile non inizializzata!";
     }
-
-    /**
-     * Constructs an instance of <code>VariableNotInitializedException</code>
-     * with the specified detail message.
-     *
-     * @param msg the detail message.
-     */
-    
 }
