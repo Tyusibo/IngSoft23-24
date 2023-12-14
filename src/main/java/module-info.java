28cba660a0624bd3ebf47bab9ba44d093ab46e5b
rmanna9 <r.manna9@studenti.unisa.it>
2023-12-14 16:30:21 +0100

    code cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module unisa.ingsoft {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    
+    
 
     opens unisa.ingsoft to javafx.fxml;
+    exports unisa.ingsoft.ComplexNumber;
     exports unisa.ingsoft;
+    
+
 }
