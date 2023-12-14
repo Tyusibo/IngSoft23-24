3ebd75d3b8e4ed1fd0863216587f454684bc22be
AndreaPio <andreapiovicinanza@gmail.com>
2023-12-14 16:52:50 +0100

    parsetests

--- src/test/java/unisa/ingsoft/ComplexNumber/ComplexNumberTest.java
+++ src/test/java/unisa/ingsoft/ComplexNumber/ComplexNumberTest.java
@@ -829,4 +829,29 @@ double delta=0.001;
         assertEquals(0,result.getIm());
     }
 
+    @Test //stringa troppo grande negativa ma non abbastanza per negative infinity(dato che i test di prima coprono parte reale e parte immaginaria testo solo su parte reale)
+    public void testParse41() throws SyntaxException{
+        String instance = "-179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180915399881250404026184129999989";
+        ComplexNumber result = ComplexNumber.parseComplexNumber(instance);
+        assertEquals(-1*(Double.MAX_VALUE),result.getRe());
+        assertEquals(0,result.getIm());
+    }
+
+    @Test //stringa troppo grande che rappresenta un numero molto piccolo (dato che i test di prima coprono parte reale e parte immaginaria testo solo su parte reale)
+    public void testParse42() throws SyntaxException{
+        String instance = "0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000025";
+        ComplexNumber result = ComplexNumber.parseComplexNumber(instance);
+        assertEquals(Double.MIN_VALUE,result.getRe());
+        assertEquals(0,result.getIm());
+    }
+
+    @Test //stringa troppo grande che rappresenta un numero troppo piccolo(dato che i test di prima coprono parte reale e parte immaginaria testo solo su parte reale)
+    public void testParse43() throws SyntaxException{
+        String instance = "0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000025";
+        ComplexNumber result = ComplexNumber.parseComplexNumber(instance);
+        assertEquals(0,result.getRe());
+        assertEquals(0,result.getIm());
+    }
+
+
 }
